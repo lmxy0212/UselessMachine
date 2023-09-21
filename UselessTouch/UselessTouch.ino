666996da6dde3e55055dbff7d9a2c5265408794e
Manxueying Li <manxueying.li@columbia.edu>
2023-09-21 10:17:13 -0400

    ok use the regular ESP32 instead. initial setup...

--- UselessTouch/UselessTouch.ino
+++ UselessTouch/UselessTouch.ino
@@ -1,42 +1,44 @@
 #include "Arduino.h"
-#include "heltec.h"
-
 
 int touchReading = 0;
 
+
+
 void setup(){
-  Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, true /*Serial Enable*/);
-  Heltec.display->flipScreenVertically();
-  Heltec.display->setFont(ArialMT_Plain_24);
+  Serial.begin(9600);
 }
 
-void drawText() {
-    Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
-    Heltec.display->setFont(ArialMT_Plain_24);
-//    Heltec.display->drawString(0, 26, "Hello world");
-    touchReading = touchRead(touchRead(T0));
-    if(touchReading < 10){
-      Heltec.display->drawString(0, 26, "Touching");
-    }else{
-      Heltec.display->drawString(0, 26, "NOT Touching");
-    }
-    Serial.print("Touch0 value is = ");
-    Serial.println(touchReading);
-}
-
+//draw a random action
+void randomAction(){
+  switch (randNumber) {
+    case 1:
+      // statements
+      break;
+    case 2:
+      // statements
+      break;
+     case 3:
+      // statements
+      break;
+     case 4:
+      // statements
+      break;
+    default:
+      // statements
+      break;
+  }
 void loop(){
+  randNumber = random(1,5);
+  Serial.println(randNumber);
+  touchReading = touchRead(4);
+  Serial.println(touchReading);
+  if(touchReading < 20){
+    randomAction();
+  }
+}
+
   
 
-
-    // clear the display
-  Heltec.display->clear();
-  // draw the current demo method
-  drawText();
-
-  Heltec.display->setTextAlignment(TEXT_ALIGN_RIGHT);
-  Heltec.display->drawString(10, 128, String(millis()));
-  // write the buffer to the display
-  Heltec.display->display();
 
   delay(10);
 }
